7a592fdca14c7d33a61d76227e6db2407cce3795
ChenJY <3220105455@zju.edu.cn>
2024-05-26 10:04:06 +0800

    Finish recovery manager

--- src/include/recovery/log_rec.h
+++ src/include/recovery/log_rec.h
@@ -22,43 +22,36 @@ enum class LogRecType {
 using KeyType = std::string;
 using ValType = int32_t;
 
+/**
+ * Implement by chenjy
+ */
 struct LogRec {
   LogRec() = default;
-
-  LogRec(LogRecType type, lsn_t lsn, txn_id_t txn_id, lsn_t prev_lsn)
-      : type_(type), lsn_(lsn), txn_id_(txn_id), prev_lsn_(prev_lsn) {}
+  LogRec(LogRecType type, lsn_t lsn, lsn_t prev_lsn, txn_id_t txn_id,
+         KeyType key, ValType val, KeyType new_key, ValType new_val)
+    : type_(type),
+      lsn_(lsn),
+      prev_lsn_(prev_lsn),
+      txn_id_(txn_id),
+      key_(std::move(key)),
+      val_(val),
+      new_key_(std::move(new_key)),
+      new_val_(new_val) {}
 
   LogRecType type_{LogRecType::kInvalid};
   lsn_t lsn_{INVALID_LSN};
-  txn_id_t txn_id_{INVALID_TXN_ID};
   lsn_t prev_lsn_{INVALID_LSN};
-  /* used for insert only */
-  KeyType ins_key_{};
-  ValType ins_val_{};
-  /* used for delete only */
-  KeyType del_key_{};
-  ValType del_val_{};
-  /* used for update only */
-  KeyType old_key_{};
-  ValType old_val_{};
-  KeyType new_key_{};
-  ValType new_val_{};
+  txn_id_t txn_id_{INVALID_TXN_ID};
+
+  KeyType key_;
+  ValType val_;
+
+  KeyType new_key_;
+  ValType new_val_;
 
   /* used for testing only */
   static std::unordered_map<txn_id_t, lsn_t> prev_lsn_map_;
   static lsn_t next_lsn_;
-
-  static lsn_t GetAndUpdatePrevLSN(txn_id_t txn_id, lsn_t cur_lsn) {
-    auto iter = prev_lsn_map_.find(txn_id);
-    auto prev_lsn = INVALID_LSN;
-    if (iter != prev_lsn_map_.end()) {
-      prev_lsn = iter->second;
-      iter->second = cur_lsn;
-    } else {
-      prev_lsn_map_.emplace(txn_id, cur_lsn);
-    }
-    return prev_lsn;
-  }
 };
 
 std::unordered_map<txn_id_t, lsn_t> LogRec::prev_lsn_map_ = {};
@@ -66,45 +59,69 @@ lsn_t LogRec::next_lsn_ = 0;
 
 typedef std::shared_ptr<LogRec> LogRecPtr;
 
-static LogRecPtr CreateInsertLog(txn_id_t txn_id, KeyType ins_key, ValType ins_val) {
-  lsn_t lsn = LogRec::next_lsn_++;
-  auto log = std::make_shared<LogRec>(LogRecType::kInsert, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
-  log->ins_key_ = std::move(ins_key);
-  log->ins_val_ = ins_val;
-  return log;
+/**
+ * Implement by chenjy
+ */
+static LogRecPtr CreateLog(LogRecType type, txn_id_t txn_id,
+                           [[maybe_unused]]const KeyType& key = "",     [[maybe_unused]]ValType val = 0,
+                           [[maybe_unused]]const KeyType& new_key = "", [[maybe_unused]]ValType new_val = 0)
+{
+  lsn_t prev_lsn = INVALID_LSN;
+  // Get and update pre_lsn
+  auto prev_txn = LogRec::prev_lsn_map_.find(txn_id);
+  // If find last txn, update lsn
+  if (prev_txn != LogRec::prev_lsn_map_.end()) {
+    prev_lsn = prev_txn->second;
+    prev_txn->second = LogRec::next_lsn_;
+  } else {
+    // If not find, add new lsn
+    LogRec::prev_lsn_map_.emplace(txn_id, LogRec::next_lsn_);
+  }
+  return std::make_shared<LogRec>(type, LogRec::next_lsn_++, prev_lsn, txn_id, key, val, new_key, new_val);
 }
 
-static LogRecPtr CreateDeleteLog(txn_id_t txn_id, KeyType del_key, ValType del_val) {
-  lsn_t lsn = LogRec::next_lsn_++;
-  auto log = std::make_shared<LogRec>(LogRecType::kDelete, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
-  log->del_key_ = std::move(del_key);
-  log->del_val_ = del_val;
-  return log;
+/**
+ * Implement by chenjy
+ */
+static LogRecPtr CreateInsertLog(txn_id_t txn_id, const KeyType& ins_key, ValType ins_val) {
+  return CreateLog(LogRecType::kInsert, txn_id, ins_key, ins_val);
 }
 
-static LogRecPtr CreateUpdateLog(txn_id_t txn_id, KeyType old_key, ValType old_val, KeyType new_key, ValType new_val) {
-  lsn_t lsn = LogRec::next_lsn_++;
-  auto log = std::make_shared<LogRec>(LogRecType::kUpdate, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
-  log->old_key_ = std::move(old_key);
-  log->old_val_ = old_val;
-  log->new_key_ = std::move(new_key);
-  log->new_val_ = new_val;
-  return log;
+/**
+ * Implement by chenjy
+ */
+static LogRecPtr CreateDeleteLog(txn_id_t txn_id, const KeyType& del_key, ValType del_val) {
+  return CreateLog(LogRecType::kDelete, txn_id, del_key, del_val);
 }
 
+/**
+ * Implement by chenjy
+ */
+static LogRecPtr CreateUpdateLog(txn_id_t txn_id, const KeyType& old_key, ValType old_val,
+                                 const KeyType& new_key, ValType new_val)
+{
+  return CreateLog(LogRecType::kUpdate, txn_id, old_key, old_val, new_key, new_val);
+}
+
+/**
+ * Implement by chenjy
+ */
 static LogRecPtr CreateBeginLog(txn_id_t txn_id) {
-  lsn_t lsn = LogRec::next_lsn_++;
-  return std::make_shared<LogRec>(LogRecType::kBegin, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
+  return CreateLog(LogRecType::kBegin, txn_id);
 }
 
+/**
+ * Implement by chenjy
+ */
 static LogRecPtr CreateCommitLog(txn_id_t txn_id) {
-  lsn_t lsn = LogRec::next_lsn_++;
-  return std::make_shared<LogRec>(LogRecType::kCommit, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
+  return CreateLog(LogRecType::kCommit, txn_id);
 }
 
+/**
+ * Implement by chenjy
+ */
 static LogRecPtr CreateAbortLog(txn_id_t txn_id) {
-  lsn_t lsn = LogRec::next_lsn_++;
-  return std::make_shared<LogRec>(LogRecType::kAbort, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
+  return CreateLog(LogRecType::kAbort, txn_id);
 }
 
 #endif  // MINISQL_LOG_REC_H
--- src/include/recovery/recovery_manager.h
+++ src/include/recovery/recovery_manager.h
@@ -1,196 +1,140 @@
-//#ifndef MINISQL_RECOVERY_MANAGER_H
-//#define MINISQL_RECOVERY_MANAGER_H
-//
-//#include <map>
-//#include <unordered_map>
-//#include <vector>
-//
-//#include "recovery/log_rec.h"
-//
-//using KvDatabase = std::unordered_map<KeyType, ValType>;
-//using ATT = std::unordered_map<txn_id_t, lsn_t>;
-//
-//struct CheckPoint {
-//  lsn_t checkpoint_lsn_{INVALID_LSN};
-//  ATT active_txns_{};
-//  KvDatabase persist_data_{};
-//
-//  inline void AddActiveTxn(txn_id_t txn_id, lsn_t last_lsn) { active_txns_[txn_id] = last_lsn; }
-//
-//  inline void AddData(KeyType key, ValType val) { persist_data_.emplace(std::move(key), val); }
-//};
-//
-//class RecoveryManager {
-// public:
-//  void Init(CheckPoint &last_checkpoint) {
-//    persist_lsn_ = last_checkpoint.checkpoint_lsn_;
-//    active_txns_ = std::move(last_checkpoint.active_txns_);
-//    data_ = std::move(last_checkpoint.persist_data_);
-//  }
-//
-//  void RedoPhase() {
-//    auto iter = log_recs_.begin();
-//    // starts from checkpoint_lsn
-//    for (; iter != log_recs_.end() && iter->first < persist_lsn_; ++iter) {
-//    }
-//    for (; iter != log_recs_.end(); ++iter) {
-//      auto &log_rec = *(iter->second);
-//      active_txns_[log_rec.txn_id_] = log_rec.lsn_;
-//      switch (log_rec.type_) {
-//        case LogRecType::kInvalid:
-//          break;
-//        case LogRecType::kInsert:
-//          data_[log_rec.ins_key_] = log_rec.ins_val_;
-//          break;
-//        case LogRecType::kDelete:
-//          data_.erase(log_rec.del_key_);
-//          break;
-//        case LogRecType::kUpdate:
-//          data_.erase(log_rec.old_key_);
-//          data_[log_rec.new_key_] = log_rec.new_val_;
-//          break;
-//        case LogRecType::kBegin:
-//          break;
-//        case LogRecType::kCommit:
-//          active_txns_.erase(log_rec.txn_id_);
-//          break;
-//        case LogRecType::kAbort:
-//          Rollback(log_rec.txn_id_);
-//          active_txns_.erase(log_rec.txn_id_);
-//          break;
-//      }
-//    }
-//  }
-//
-//  void UndoPhase() {
-//    for (const auto &[txn_id, _] : active_txns_) {
-//      Rollback(txn_id);
-//    }
-//    active_txns_.clear();
-//  }
-//
-//  void Rollback(txn_id_t txn_id) {
-//    auto last_log_lsn = active_txns_[txn_id];
-//    while (last_log_lsn != INVALID_LSN) {
-//      auto log_rec = log_recs_[last_log_lsn];
-//      if (log_rec == nullptr) break;
-//      switch (log_rec->type_) {
-//        case LogRecType::kInsert:
-//          data_.erase(log_rec->ins_key_);
-//          break;
-//        case LogRecType::kDelete:
-//          data_[log_rec->del_key_] = log_rec->del_val_;
-//          break;
-//        case LogRecType::kUpdate:
-//          data_.erase(log_rec->new_key_);
-//          data_[log_rec->old_key_] = log_rec->old_val_;
-//          break;
-//        default:
-//          break;
-//      }
-//      last_log_lsn = log_rec->prev_lsn_;
-//    }
-//  }
-//
-//  // used for test only
-//  void AppendLogRec(LogRecPtr log_rec) { log_recs_.emplace(log_rec->lsn_, log_rec); }
-//
-//  // used for test only
-//  inline KvDatabase &GetDatabase() { return data_; }
-//
-// private:
-//  std::map<lsn_t, LogRecPtr> log_recs_{};
-//  lsn_t persist_lsn_{INVALID_LSN};
-//  ATT active_txns_{};
-//  KvDatabase data_{};  // all data in database
-//};
-//
-//#endif  // MINISQL_RECOVERY_MANAGER_H
+#ifndef MINISQL_RECOVERY_MANAGER_H
+#define MINISQL_RECOVERY_MANAGER_H
 
-#ifndef MINISQL_LOG_REC_H
-#define MINISQL_LOG_REC_H
-
+#include <map>
 #include <unordered_map>
-#include <utility>
+#include <vector>
 
-#include "common/config.h"
-#include "common/rowid.h"
-#include "record/row.h"
+#include "recovery/log_rec.h"
 
-enum class LogRecType {
-  kInvalid,
-  kInsert,
-  kDelete,
-  kUpdate,
-  kBegin,
-  kCommit,
-  kAbort,
+using KvDatabase = std::unordered_map<KeyType, ValType>;
+using ATT = std::unordered_map<txn_id_t, lsn_t>;
+
+struct CheckPoint {
+  lsn_t checkpoint_lsn_{INVALID_LSN};
+  ATT active_txns_{};
+  KvDatabase persist_data_{};
+
+  inline void AddActiveTxn(txn_id_t txn_id, lsn_t last_lsn) { active_txns_[txn_id] = last_lsn; }
+
+  inline void AddData(KeyType key, ValType val) { persist_data_.emplace(std::move(key), val); }
 };
 
-// used for testing only
-using KeyType = std::string;
-using ValType = int32_t;
+class RecoveryManager {
+ public:
+  /**
+    * Implement by chenjy
+   */
+  void Init(CheckPoint &last_checkpoint) {
+    persist_lsn_ = last_checkpoint.checkpoint_lsn_;
+    active_txns_ = last_checkpoint.active_txns_;
+    data_ = last_checkpoint.persist_data_;
+  }
 
-/**
- * TODO: Student Implement
- */
-struct LogRec {
-  LogRec() = default;
+  /**
+    * Implement by chenjy
+   */
+  void RedoPhase() {
+    for(const auto& log_iter: log_recs_) {
+      if (log_iter.first < persist_lsn_) continue;
+      LogRecPtr cur_log = log_iter.second;
+      active_txns_[cur_log->txn_id_] = cur_log->lsn_;
+      switch (cur_log->type_) {
+        case LogRecType::kInvalid:
+          break;
+        case LogRecType::kInsert:
+          data_[cur_log->key_] = cur_log->val_;
+          break;
+        case LogRecType::kDelete:
+          if (data_[cur_log->key_] == cur_log->val_)
+            data_.erase(cur_log->key_);
+          else
+            LOG(WARNING) << "Repeated deletion" << std::endl;
+          break;
+        case LogRecType::kUpdate:
+          if (data_[cur_log->key_] == cur_log->val_)
+            data_.erase(cur_log->key_);
+          else
+            LOG(WARNING) << "Old value has been modified" << std::endl;
+          data_[cur_log->new_key_] = cur_log->new_val_;
+          break;
+        case LogRecType::kBegin:
+          break;
+        case LogRecType::kCommit:
+          active_txns_.erase(cur_log->txn_id_);
+          break;
+        case LogRecType::kAbort:
+          // Rollback
+          lsn_t p_lsn = cur_log->prev_lsn_;
+          while (p_lsn != INVALID_LSN) {
+            auto p = log_recs_[p_lsn];
+            RollBack(p);
+            p_lsn = p->prev_lsn_;
+          }
+          active_txns_.erase(cur_log->txn_id_);
+          break;
+      }
+    }
+  }
 
-  LogRecType type_{LogRecType::kInvalid};
-  lsn_t lsn_{INVALID_LSN};
-  lsn_t prev_lsn_{INVALID_LSN};
+  /**
+    * Implement by chenjy
+   */
+  void UndoPhase() {
+    for (const auto txn_iter : active_txns_) {
+      lsn_t lsn = txn_iter.second;
+      lsn_t lsn_p = lsn;
+      while(lsn_p != INVALID_LSN) {
+        LogRecPtr p = log_recs_[lsn_p];
+        RollBack(p);
+        lsn_p = p->prev_lsn_;
+      }
+    }
+    // Clear active txn
+    active_txns_.clear();
+  }
 
-  /* used for testing only */
-  static std::unordered_map<txn_id_t, lsn_t> prev_lsn_map_;
-  static lsn_t next_lsn_;
+  // used for test only
+  void AppendLogRec(LogRecPtr log_rec) { log_recs_.emplace(log_rec->lsn_, log_rec); }
+
+  // used for test only
+  inline KvDatabase &GetDatabase() { return data_; }
+
+ private:
+  std::map<lsn_t, LogRecPtr> log_recs_{};
+  lsn_t persist_lsn_{INVALID_LSN};
+  ATT active_txns_{};
+  KvDatabase data_{};  // all data in database
+
+  /**
+    * Implement by chenjy
+   */
+  void RollBack(const LogRecPtr& p) {
+    switch (p->type_) {
+      case LogRecType::kInsert:
+        // Delete inserted one
+        if(data_[p->key_] == p->val_)
+          data_.erase(p->key_);
+        else
+          LOG(WARNING) << "Rollback unknown error" << std::endl;
+        break;
+      case LogRecType::kUpdate:
+        // Remove update
+        if(data_[p->new_key_] == p->new_val_)
+          data_.erase(p->new_key_);
+        else
+          LOG(WARNING) << "Rollback unknown error" << std::endl;
+        data_[p->key_] = p->val_;
+        break;
+      case LogRecType::kDelete:
+        // Insert deleted one
+        data_[p->key_] = p->val_;
+        break;
+      default:
+        // Nothing to do.
+        break;
+    }
+  }
 };
 
-std::unordered_map<txn_id_t, lsn_t> LogRec::prev_lsn_map_ = {};
-lsn_t LogRec::next_lsn_ = 0;
-
-typedef std::shared_ptr<LogRec> LogRecPtr;
-
-/**
- * TODO: Student Implement
- */
-static LogRecPtr CreateInsertLog(txn_id_t txn_id, KeyType ins_key, ValType ins_val) {
-  return nullptr;
-}
-
-/**
- * TODO: Student Implement
- */
-static LogRecPtr CreateDeleteLog(txn_id_t txn_id, KeyType del_key, ValType del_val) {
-  return nullptr;
-}
-
-/**
- * TODO: Student Implement
- */
-static LogRecPtr CreateUpdateLog(txn_id_t txn_id, KeyType old_key, ValType old_val, KeyType new_key, ValType new_val) {
-  return nullptr;
-}
-
-/**
- * TODO: Student Implement
- */
-static LogRecPtr CreateBeginLog(txn_id_t txn_id) {
-  return nullptr;
-}
-
-/**
- * TODO: Student Implement
- */
-static LogRecPtr CreateCommitLog(txn_id_t txn_id) {
-  return nullptr;
-}
-
-/**
- * TODO: Student Implement
- */
-static LogRecPtr CreateAbortLog(txn_id_t txn_id) {
-  return nullptr;
-}
-
-#endif  // MINISQL_LOG_REC_H
+#endif  // MINISQL_RECOVERY_MANAGER_H
